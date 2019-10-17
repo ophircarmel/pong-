36876d57cad6b84a79fb0545c1a799f913ddec1e
yehonatanHarmatz <47951976+yehonatanHarmatz@users.noreply.github.com>
2019-10-17 13:14:22 +0300

    Add files via upload

--- Assets/setting.cs
+++ Assets/setting.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class setting : MonoBehaviour
 {
-    public string Player1Down = "down";
-    public string Player1Up = "up";
-    public string Player2Down = "s";
-    public string Player2Up = "w";
-    public bool twoPlayers = true;
+    public static string Player1Down = "down";
+    public static string Player1Up = "up";
+    public static string Player2Down = "s";
+    public static string Player2Up = "w";
+    public static bool twoPlayers = true;
+    public InputField p1up;
+    public InputField p1down;
+    public InputField p2up;
+    public InputField p2down;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +24,21 @@ public class setting : MonoBehaviour
     void Update()
     {
 
+    }
+    public void setPlayer1Up()
+    {
+        Player1Up = p1up.text;
+    }
+    public void setPlayer2Up()
+    {
+        Player2Up = p2up.text;
+    }
+    public void setPlayer1Down()
+    {
+        Player1Down = p1down.text;
+    }
+    public void setPlayer2Down()
+    {
+        Player2Down = p2down.text;
     }
 }
