51eb4b5b7a15bcb24f8c3406f0e450dfb38a6a55
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2019-10-18 11:31:45 +0300

    Update ExitOnClick.cs

--- Assets/ExitOnClick.cs
+++ Assets/ExitOnClick.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 public class ExitOnClick : MonoBehaviour
 {
+    // <summary>
+    // Handle the case of clicking exit button.
+    // </summary>
     public void ExitGame()
     {
+        // Close the application.
         Application.Quit();
     }
 }
