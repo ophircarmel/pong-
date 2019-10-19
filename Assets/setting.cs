03b141ace2883b6c19a82617fb562bcd49059259
yehonatanHarmatz <47951976+yehonatanHarmatz@users.noreply.github.com>
2019-10-20 00:38:13 +0300

    Add files via upload

--- Assets/setting.cs
+++ Assets/setting.cs
@@ -16,6 +16,7 @@ public class setting : MonoBehaviour
     public InputField p2down;
 
     public static float HardLevel;
+    internal static int score = 3;
 
     // Start is called before the first frame update
     void Start()
